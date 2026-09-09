e1a49fd1779326f65cc67007c4bafb9bb5d62f96
tahodev <85001874+tahodev@users.noreply.github.com>
2026-09-09 17:41:46 +0900

    Update 4.3.2-test.js

--- test/4.3.2-test.js
+++ test/4.3.2-test.js
@@ -18,6 +18,22 @@ tester.run("4.3.2.大かっこ［］", rule, {
             ]
         },
         {
+            // 開きかっこが日本語に隣接していない位置（行頭や数字の後ろ）でも修正する
+            // https://github.com/textlint-ja/textlint-rule-preset-JTF-style/issues/93
+            text: "1. [ファイル]をクリック",
+            output: "1. ［ファイル］をクリック",
+            errors: [
+                {
+                    message: "半角の大かっこ[]が使用されています。全角のかっこ［］を使用してください。",
+                    column: 4
+                },
+                {
+                    message: "半角の大かっこ[]が使用されています。全角のかっこ［］を使用してください。",
+                    column: 9
+                }
+            ]
+        },
+        {
             // 半角かっこ Markdown的に意味を持つので片方ずつ
             text: "半角]かっこ",
             output: "半角］かっこ",
